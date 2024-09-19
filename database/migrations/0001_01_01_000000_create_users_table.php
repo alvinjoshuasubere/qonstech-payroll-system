023dba98969006f072373ea5604f8b386de1253d
Machys <134110423+Lyts0816@users.noreply.github.com>
2024-09-19 21:43:05 +0800

    new_Updated

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name', 50);
+            $table->string('Username', 50);
             $table->string('email')->unique()->nullable();
             $table->string('password', 100);
             $table->string('Role', 20);
